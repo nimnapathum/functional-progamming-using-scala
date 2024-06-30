13b2fef972b38da4d009b1db8faa8f5acba51f29
Nimnapathum <pathumeshan123@gmail.com>
2024-06-30 23:30:01 +0530

    add some extra function to 4.scala

--- LabSession03/4.scala
+++ LabSession03/4.scala
@@ -8,9 +8,21 @@ object question_3_04{
         } 
         sumofevens
     }
+
+    def sumOfEvens_using_recursion(numlist: List[Int]):Int={
+        if (numlist.isEmpty) 0
+        else{
+            val first = numlist.head
+            if (first % 2 == 0) first + sumOfEvens_using_recursion(numlist.tail)
+            else sumOfEvens_using_recursion(numlist.tail)
+        }
+    }
+
     def main(args: Array[String]):Unit={
         val numList: List[Int] = List( 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8)
         val sumofevens = sumOfEvens(numList)
-        println(sumofevens)
+        println(s"Sum of the List : $sumofevens")
+        println(s"Sum of the List using recursion: ${sumOfEvens_using_recursion(numList)}")
+        
     }
 }
