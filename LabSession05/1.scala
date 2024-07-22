17543f5bf1e091dec25902b0e3020ff4c6d792aa
Nimnapathum <pathumeshan123@gmail.com>
2024-07-22 14:39:16 +0530

    commit this

--- LabSession05/1.scala
+++ LabSession05/1.scala
@@ -1,34 +1,35 @@
-object question_5_01{
-    def getProductList():List[String]={
-        var products: List[String] = List()
-        var product: String = ""
-        var i = 1
-        product = scala.io.StdIn.readLine(s"Product 1: ")
-        while(!(product == "done")){
-            products = products :+ product
-            product = scala.io.StdIn.readLine(s"Product ${i+1}: ")
-            i += 1
-        }
-        products
-    }
+object question_5_01 {
+  def getProductList(): List[String] = {
+    var products: List[String] = List()
+    var product: String = ""
+    var i = 1
+    product = scala.io.StdIn.readLine(s"Product 1: ")
+    while (!(product == "done")) {
 
-    def printProductList(list: List[String]):Unit={
-        println("\nProduct List----------")
-        var i = 1
-        for(product <- list){
-            println(s"$i: $product")
-            i+=1
-        }
+      products = products :+ product
+      product = scala.io.StdIn.readLine(s"Product ${i + 1}: ")
+      i += 1
     }
+    products
+  }
 
-    def getTotalProducts(list: List[String]):Int={
-        list.length
+  def printProductList(list: List[String]): Unit = {
+    println("\nProduct List----------")
+    var i = 1
+    for (product <- list) {
+      println(s"$i: $product")
+      i += 1
     }
+  }
 
-    def main(args: Array[String]):Unit={
-        println("--------========[Product List]========--------")
-        var list1 = getProductList()
-        printProductList(list1)
-        println(s"No of products in the list: ${getTotalProducts(list1)}")
-    }
+  def getTotalProducts(list: List[String]): Int = {
+    list.length
+  }
+
+  def main(args: Array[String]): Unit = {
+    println("--------========[Product List]========--------")
+    var list1 = getProductList()
+    printProductList(list1)
+    println(s"No of products in the list: ${getTotalProducts(list1)}")
+  }
 }
