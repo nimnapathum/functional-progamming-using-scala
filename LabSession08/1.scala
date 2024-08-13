2c672426e34a08a34efb5322ef20567a70c376df
Nimnapathum <pathumeshan123@gmail.com>
2024-08-13 08:34:42 +0530

    upgrade code

--- LabSession08/1.scala
+++ LabSession08/1.scala
@@ -4,15 +4,37 @@ object question_8_01{
         text.map(c => (c + shifter).toChar)
     }
 
+    def encrypt_alpha(text: String , shifter: Int): String = {
+        text.map(c => 
+            if(c.isLetter){
+                val base = if (c.isUpper) 'A' else 'a'
+                ((c - base + shifter) % 26 + base).toChar
+            }else{
+                c
+            }
+        )
+    }
+
     def decrypt(text: String , shifter: Int): String = {
         text.map(c => (c - shifter).toChar)
+    }
+
+    def decrypt_alpha(text: String , shifter: Int): String = {
+        text.map(c => 
+            if(c.isLetter){
+                val base = if (c.isUpper) 'A' else 'a'
+                ((c - base - shifter) % 26 + base).toChar
+            }else{
+                c
+            }
+        )
     }
 
     def main(args: Array[String]): Unit={
         val text = "Hello Scala"
         println(s"Original Text : ${text}")
-        val encrypted_text = encrypt(text , 1) 
+        val encrypted_text = encrypt_alpha(text , 1) 
         println(s"Encrypted Text : ${encrypted_text}")
-        println(s"Decrypted Text : ${decrypt(encrypted_text , 1)}")
+        println(s"Decrypted Text : ${decrypt_alpha(encrypted_text , 1)}")
     }
 }
